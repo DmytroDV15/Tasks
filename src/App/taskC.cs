1a6c9a4a19f023c712a7ee0d4c43d1cd9462b80d
DmytroDV15 <d.demchik15@gmail.com>
2025-09-04 17:32:34 +0300

    task phase C

---
+++ src/App/taskC.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Db;
+using Microsoft.EntityFrameworkCore;
+
+class taskC
+{
+    private readonly NorthwindDbContext _context;
+
+    public taskC(NorthwindDbContext context)
+    {
+        _context = context;
+    }
+    public async Task Run()
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        var lastDayOfSales = await _context.Orders
+            .AsNoTracking()
+            .MaxAsync(o => o.OrderDate);
+
+        var cutoffDate = lastDayOfSales.Value.AddMonths(-12);
+
+        var topCustomers = await _context.Customers
+            .AsNoTracking()
+            .Select(c => new
+            {
+                c.CustomerId,
+                c.CompanyName,
+                RevenueLast12Months = c.Orders
+                    .Where(o => o.OrderDate >= cutoffDate)
+                    .SelectMany(o => o.OrderDetails)
+                    .Sum(od => od.UnitPrice * od.Quantity * (1 - od.Discount)),
+
+                LastOrderDate = c.Orders
+                    .Where(o => o.OrderDate >= cutoffDate)
+                    .Max(o => (DateTime?)o.OrderDate)
+            })
+            .OrderByDescending(c => c.RevenueLast12Months)
+            .Take(10)
+            .ToListAsync();
+
+        stopwatch.Stop();
+
+        foreach (var c in topCustomers)
+        {
+            Console.WriteLine(
+                $"CustomerID: {c.CustomerId}, " +
+                $"Company: {c.CompanyName}, " +
+                $"Revenue: {c.RevenueLast12Months:F2}, " +
+                $"LastOrder: {c.LastOrderDate}");
+        }
+
+        Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
+    }
+}
---
+++ src/Migrations/20250903143141_AddOrderIndexes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "CREATE INDEX IX_Orders_CustomerId_OrderDate ON Orders (CustomerID, OrderDate DESC)"
+            );
+
+            migrationBuilder.Sql(
+                "CREATE INDEX IX_OrderDetails_OrderId ON [Order Details] (OrderID)"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX IX_Orders_CustomerId_OrderDate ");
+            migrationBuilder.Sql("DROP INDEX IX_OrderDetails_OrderId ");
+        }
+    }
+}

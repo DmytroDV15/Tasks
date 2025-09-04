3d23b2e80eb4214b9c28ebe327b45ebe5fb7ecb6
DmytroDV15 <d.demchik15@gmail.com>
2025-09-04 17:33:20 +0300

    task 2 phase C

---
+++ src/App/taskC2.cs
@@ -0,0 +1,42 @@
+﻿using Db;
+using Microsoft.EntityFrameworkCore;
+
+
+class taskC2
+{
+    private readonly NorthwindDbContext _context;
+    public taskC2(NorthwindDbContext context)
+    {
+        _context = context;
+    }
+    public async Task Run()
+    {
+        var newOrder = new Order
+        {
+            CustomerId = "ALFKI",
+            EmployeeId = 1,
+            OrderDate = DateTime.UtcNow
+        };
+
+        _context.Orders.Add(newOrder);
+        await _context.SaveChangesAsync();
+
+        Console.WriteLine($"CreatedAt: {newOrder.CreatedAt}, ModifiedAt: {newOrder.ModifiedAt}");
+
+        newOrder.Freight = 123;
+        await _context.SaveChangesAsync();
+
+        Console.WriteLine($"After modification, ModifiedAt: {newOrder.ModifiedAt}");
+
+        newOrder.IsDeleted = true;
+        await _context.SaveChangesAsync();
+
+        var orderFromDb = await _context.Orders
+            .AsNoTracking()
+            .FirstOrDefaultAsync(o => o.OrderId == newOrder.OrderId);
+
+        Console.WriteLine(orderFromDb == null
+            ? "Soft-deleted order is filtered out"
+            : "Order is still visible (ERROR)");
+    }
+}
---
+++ src/Migrations/20250904084906_AddAuditing.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Orders",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Orders");
+        }
+    }
+}

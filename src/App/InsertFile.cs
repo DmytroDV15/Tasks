40adc3c26299bb4da1d84714c3f30b1fb2a45257
DmytroDV15 <d.demchik15@gmail.com>
2025-09-04 17:30:38 +0300

    task11

---
+++ src/App/InsertFile.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace task11;
+public class InsertFile
+{
+    public void CreateFile()
+    {
+        var random = new Random();
+        var sb = new StringBuilder();
+        var totalRows = 100000;
+
+        sb.AppendLine("ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued");
+
+        for (int i = 1; i <= totalRows; i++)
+        {
+            var productID = i;
+            var productName = $"Product {i}";
+            var supplierID = random.Next(1, 30);
+            var categoryID = random.Next(1, 9);
+            var quantityPerUnit = $"{random.Next(1, 100)} pcs";
+            var unitPrice = (random.NextDouble() * 100).ToString("F2", CultureInfo.InvariantCulture);
+            var unitsInStock = random.Next(0, 500);
+            var unitsOnOrder = random.Next(0, 100);
+            var reorderLevel = random.Next(0, 50);
+            var discontinued = random.Next(0, 2);
+
+            sb.AppendLine($"{productID},{productName},{supplierID},{categoryID},{quantityPerUnit},{unitPrice},{unitsInStock},{unitsOnOrder},{reorderLevel},{discontinued}");
+        }
+
+        File.WriteAllText("products.csv", sb.ToString());
+    }
+}
---
+++ src/App/task11.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using Db;
+
+public static class Task11
+{
+    public static async Task Run()
+    {
+        var insertFile = new task11.InsertFile();
+
+        insertFile.CreateFile();
+
+        var filePath = "products.csv";
+
+        var stopwatch = Stopwatch.StartNew();
+
+        var lines = File.ReadAllLines(filePath).Skip(1);
+        var products = lines.Select(line =>
+        {
+            var columns = line.Split(',');
+            return new Product
+            {
+                ProductName = columns[1],
+                SupplierId = int.Parse(columns[2]),
+                CategoryId = int.Parse(columns[3]),
+                QuantityPerUnit = columns[4],
+                UnitPrice = double.Parse(columns[5], CultureInfo.InvariantCulture),
+                UnitsInStock = int.Parse(columns[6]),
+                UnitsOnOrder = int.Parse(columns[7]),
+                ReorderLevel = int.Parse(columns[8]),
+                Discontinued = columns[9] == "1"
+            };
+        }).ToList();
+
+        using (var context = new NorthwindDbContext())
+        using (var transaction = context.Database.BeginTransaction())
+        {
+            context.Products.AddRange(products);
+            context.SaveChanges();
+            transaction.Rollback();
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"Inserted {products.Count} products in {stopwatch.Elapsed.TotalSeconds:F2} sec");
+
+        var stopwatch2 = Stopwatch.StartNew();
+
+        var batchSize = 5000;
+
+        using (var context = new NorthwindDbContext())
+        using (var transaction = context.Database.BeginTransaction())
+        {
+            for (int i = 0; i < products.Count; i += batchSize)
+            {
+                var batch = products.Skip(i).Take(batchSize).ToList();
+                context.Products.AddRange(batch);
+                context.SaveChanges();
+                context.ChangeTracker.Clear();
+            }
+
+            transaction.Rollback();
+        }
+
+
+        stopwatch2.Stop();
+        Console.WriteLine($"Inserted {products.Count} products in batches of {batchSize} in {stopwatch2.Elapsed.TotalSeconds:F2} sec");
+    }
+}
